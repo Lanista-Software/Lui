7f8842f842edc96c53e9bffbce28074fa7db5a0c
serkankorkac <korkacserkan@gmail.com>
2020-11-23 17:04:40 +0300

    ProductpreviewCardleft added

--- src/stories/cards/ProductPreviewCard.stories.js
+++ src/stories/cards/ProductPreviewCard.stories.js
@@ -1,12 +1,18 @@
 // import { boolean, select, text } from "@storybook/addon-knobs";
 import ProductpreviewCardtop from "../../components/Cards/ProductpreviewCard/ProductpreviewCardtop.vue";
+import ProductpreviewCardleft from "../../components/Cards/ProductpreviewCard/ProductpreviewCardleft.vue";
 export default {
-  title: "Cards/ProductpreviewCardtop",
-  component: { ProductpreviewCardtop }
+  title: "Cards/ProductpreviewCard",
+  component: { ProductpreviewCardtop, ProductpreviewCardleft }
 };
 
 export const ProductpreviewCard_top = () => ({
   components: { ProductpreviewCardtop },
   template: `<ProductpreviewCardtop>
   </ProductpreviewCardtop>`
+});
+export const ProductpreviewCard_left = () => ({
+  components: { ProductpreviewCardleft },
+  template: `<ProductpreviewCardleft>
+  </ProductpreviewCardleft>`
 });
