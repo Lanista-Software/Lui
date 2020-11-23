ba89407c3e460e536dc0ba733c23b96db7b1289f
serkankorkac <korkacserkan@gmail.com>
2020-11-23 16:02:33 +0300

    ProductpreviewCardtop added

---
+++ src/components/Cards/ProductpreviewCard/ProductpreviewCardtop.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="product-preview-card-top">
+    <div class="bg-warning">
+      <span>
+        Popüler
+      </span>
+    </div>
+    <div>
+      <img src="/erbay/trendyol.png" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.product-preview-card-top {
+  display: inline-flex;
+  align-items: center;
+  width: 100%;
+  span {
+    font-size: 12px;
+    font-family: $baseFont;
+  }
+  div:nth-child(1) {
+    display: flex;
+    justify-content: flex-start;
+    padding: 8px 16px;
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+    left: 0px;
+  }
+  div:nth-child(2) {
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+    img {
+      width: 45px;
+    }
+  }
+}
+</style>
---
+++ src/stories/cards/ProductPreviewCard.stories.js
@@ -0,0 +1,12 @@
+// import { boolean, select, text } from "@storybook/addon-knobs";
+import ProductpreviewCardtop from "../../components/Cards/ProductpreviewCard/ProductpreviewCardtop.vue";
+export default {
+  title: "Cards/ProductpreviewCardtop",
+  component: { ProductpreviewCardtop }
+};
+
+export const ProductpreviewCard_top = () => ({
+  components: { ProductpreviewCardtop },
+  template: `<ProductpreviewCardtop>
+  </ProductpreviewCardtop>`
+});
