cbe72c9a9db50b38c347b4791e0d7b72bbc19845
serkankorkac <korkacserkan@gmail.com>
2020-11-25 16:22:35 +0300

    FilterGroup props added

--- src/components/FilterGroup.vue
+++ src/components/FilterGroup.vue
@@ -1,25 +1,13 @@
 <template>
   <div class="filter-group shadow radius-05 bg-light">
     <l-button
-      :bgVariant="active === 0 ? 'bg-white' : 'bg-light'"
-      textVariant="text-dark"
+      v-for="(btn, i) in buttons"
+      :key="i"
+      :bgVariant="active === i ? btn.active : btn.deactive"
+      :textVariant="btn.textVariant"
       size="sm"
-      @click="activeEmit(0)"
-      >Ödeme</l-button
-    >
-    <l-button
-      :bgVariant="active === 1 ? 'bg-white' : 'bg-light'"
-      textVariant="text-dark"
-      size="sm"
-      @click="activeEmit(1)"
-      >Teknik</l-button
-    >
-    <l-button
-      :bgVariant="active === 2 ? 'bg-white' : 'bg-light'"
-      textVariant="text-dark"
-      size="sm"
-      @click="activeEmit(2)"
-      >İletişim</l-button
+      @click="activeEmit(i)"
+      >{{ btn.text }}</l-button
     >
   </div>
 </template>
@@ -29,6 +17,12 @@ import LButton from "./LButton.vue";
 export default {
   name: "FilterGroup",
   components: { LButton },
+  props: {
+    buttons: {
+      type: Array,
+      required: true
+    }
+  },
   data() {
     return {
       active: 0
