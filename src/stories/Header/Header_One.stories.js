bc14ff663c9a18a6943d3197553b2582ccc92109
serkankorkac <korkacserkan@gmail.com>
2020-11-27 16:23:06 +0300

    mobile menu added

--- src/stories/Header/Header_One.stories.js
+++ src/stories/Header/Header_One.stories.js
@@ -1,4 +1,5 @@
 import Header from "../../components/Header/HeaderOne/HeaderOne.vue";
+import mobilemenu from "../../components/Header/HeaderOne/HeaderOneComponents/MobileMenu.vue";
 export default {
   title: "Headers/HeaderOne/HeaderOne",
   component: { Header },
@@ -10,4 +11,9 @@ export default {
 export const HeaderOne = () => ({
   components: { Header },
   template: "<Header></Header>"
+});
+
+export const mobile_menu = () => ({
+  components: { mobilemenu },
+  template: "<mobilemenu></mobilemenu>"
 });
