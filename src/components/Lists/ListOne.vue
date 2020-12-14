40f1909b43e0dc4faf0eecec866510a6c34050fe
serkankorkac <korkacserkan@gmail.com>
2020-12-14 17:44:27 +0300

    lists component added

---
+++ src/components/Lists/ListOne.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="list-one">
+    <i class="ri-shield-check-fill text-success" />
+    <p class="text-white">
+      <slot />
+    </p>
+  </div>
+</template>
+<style lang="scss" scoped>
+.list-one {
+  display: inline-flex;
+  column-gap: 1.5rem;
+  align-items: flex-start;
+}
+</style>
---
+++ src/stories/Lists/ListOne.stories.js
@@ -0,0 +1,10 @@
+import ListOne from "../../components/Lists/ListOne.vue";
+export default {
+  title: "Lists/ListOne",
+  component: ListOne
+};
+export const List_One = () => ({
+  components: { ListOne },
+  template: `<ListOne>
+  Tüm ihtiyaçlarınızı karşılayabileceğiniz en güvenilir firma.</ListOne>`
+});
