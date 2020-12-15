704da189a33b49010f7777dfd2b4fc89c1b16aea
serkankorkac <korkacserkan@gmail.com>
2020-12-15 15:30:16 +0300

    social one added

---
+++ src/stories/SocilaGroups/SocialOne.stories.js
@@ -0,0 +1,9 @@
+import SocialOne from "../../components/SocialGroups/SocialOne.vue";
+export default {
+  title: "SocialGroups/SocialOne",
+  component: SocialOne
+};
+export const Social_One = () => ({
+  components: { SocialOne },
+  template: `<SocialOne></SocialOne>`
+});
