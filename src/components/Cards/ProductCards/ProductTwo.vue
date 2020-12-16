e74cbab31c304b204f27c5a45b3e84d16de2ea49
serkankorkac <korkacserkan@gmail.com>
2020-12-16 17:44:57 +0300

    product  two hover state added

--- src/components/Cards/ProductCards/ProductTwo.vue
+++ src/components/Cards/ProductCards/ProductTwo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-bind="$attrs" class="product-two radius-05">
+  <div v-bind="$attrs" class="product-two radius-05 shadow">
     <div class="product-two-tag">
       <slot name="tag"></slot>
     </div>
@@ -21,16 +21,56 @@
         <slot name="properties"></slot>
       </div>
     </div>
+    <transition name="fade">
+      <div class="hover-pr bg-dark_09 radius-05">
+        <div class="hover-pr-top">
+          <p class="text-white">
+            Ürün fiyatlarını öğrenmek ve teklif almak için
+          </p>
+        </div>
+        <div class="hover-pr-button ">
+          <l-button size="sm" textVariant="text-white" bgVariant="bg-success"
+            >Bize ulaşın</l-button
+          >
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
+import LButton from "../../LButton.vue";
 export default {
+  components: { LButton },
   name: "Producttwo"
 };
 </script>
 
 <style scoped lang="scss">
+.hover-pr {
+  display: grid;
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  grid-template-rows: 1fr auto;
+  padding: 5px;
+  gap: 10px;
+  align-items: center;
+  &-top {
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    p {
+      width: 100%;
+    }
+  }
+  &-button {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+}
 .product-two {
   display: grid;
   padding: 12px;
@@ -73,6 +113,23 @@ export default {
     img {
       width: 100%;
       height: auto;
+    }
+  }
+}
+.product-two:hover {
+  .hover-pr {
+    opacity: 1;
+    transition: 1s;
+  }
+}
+@media ($lg) {
+  .hover-pr {
+    padding: 25px 0px;
+    gap: 20px;
+    &-top {
+      p {
+        width: 70%;
+      }
     }
   }
 }
