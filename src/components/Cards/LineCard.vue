5affbe1797accc09a2a0e21caa3a7195e66ab3d2
ABB <bayhanbayramoglu@gmail.com>
2020-12-18 18:33:37 +0300

    Line Card and BgIcon components add

---
+++ src/components/Cards/LineCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="line-card">
+    <div class="line-card-top">
+      <div class="line-card-top-icon"></div>
+      <div class="line-card-top-Title"></div>
+    </div>
+    <div class="line-card-content"></div>
+    <div class="line-card-footer"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LineCard"
+};
+</script>
+
+<style lang="scss" scoped>
+.line-card {
+  padding: 20px;
+  &-top {
+    display: flex;
+    align-items: center;
+  }
+  &-content {
+    margin-top: 20px;
+  }
+  &-footer {
+    margin-top: 20px;
+  }
+}
+</style>
---
+++ src/components/Icons/BgIcon.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    v-bind="$attrs"
+    :style="{ width: size[0] + 'px', height: size[1] + 'px' }"
+    :class="[typeSelector]"
+    class="bg-icon"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BgIcon",
+  props: {
+    circle: {
+      type: Boolean,
+      default: false
+    },
+    size: {
+      type: Array,
+      required: false
+    }
+  },
+  computed: {
+    typeSelector() {
+      return this.circle === true ? "rounded-circle" : "rounded";
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.bg-icon {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
