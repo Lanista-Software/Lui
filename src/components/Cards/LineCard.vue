064214881a769b7352927160ad7d4e5a2cb45c41
A.B.B <bayhanbayramoglu@gmail.com>
2020-12-19 12:06:32 +0300

    Line  Card is complated

--- src/components/Cards/LineCard.vue
+++ src/components/Cards/LineCard.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="line-card">
     <div class="line-card-top">
-      <div class="line-card-top-icon"></div>
-      <div class="line-card-top-Title"></div>
+      <div class="line-card-top-icon"><slot name="icon"></slot></div>
+      <div class="line-card-top-Title"><slot name="title"></slot></div>
     </div>
-    <div class="line-card-content"></div>
-    <div class="line-card-footer"></div>
+    <div class="line-card-content"><slot name="content"></slot></div>
+    <div class="line-card-footer"><slot name="footer"></slot></div>
   </div>
 </template>
 
@@ -18,8 +18,12 @@ export default {
 <style lang="scss" scoped>
 .line-card {
   padding: 20px;
+  width: 100%;
+  max-width: 275px;
+  min-width: 200px;
   &-top {
-    display: flex;
+    display: inline-flex;
+    column-gap: 20px;
     align-items: center;
   }
   &-content {
