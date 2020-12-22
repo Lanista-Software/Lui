bf017b84ea6bff710028525a1f1a6fad4a2e99c7
serkankorkac <korkacserkan@gmail.com>
2020-12-22 15:04:36 +0300

    Documentcard added

---
+++ src/stories/cards/DocumentCard.stories.js
@@ -0,0 +1,33 @@
+import DocumentCard from '../../components/Cards/DocumentCard'
+import LButton from '../../components/LButton'
+
+export default {
+  title: 'Cards/DocumentCard',
+  component: { DocumentCard }
+}
+
+export const Document_Card = () => ({
+  components: { DocumentCard, LButton },
+  template: `<DocumentCard img="https://api.lanista.com.tr/assest/images/girişimci.PNG-4-4-.jpg">
+    <template v-slot:desc>
+    <p class="fw-600">
+    Teknik Dosya
+  </p>
+  <small>
+    20.03.2021
+  </small>
+    </template>
+    <template v-slot:button>
+        <l-button bg-variant="bg-primary" size="sm" text-variant="text-white">
+        Hemen Arayın
+        </l-button>
+    </template>
+    <template v-slot:info>
+    <p class="text-white">
+    Dosyayı talep
+    <br />
+    etmek için
+  </p>
+    </template>
+  </DocumentCard>`
+})
