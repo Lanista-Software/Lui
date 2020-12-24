f5195851e17f1efef736625dee770602ccd70af0
ABB <bayhanbayramoglu@gmail.com>
2020-12-24 14:04:07 +0300

    Card folder fix

---
+++ src/stories/Cards/MapformCard.stories.js
@@ -0,0 +1,12 @@
+import MapformCard from '../../components/Cards/MapformCard'
+
+export default {
+  title: 'Cards/MapformCard',
+  component: { MapformCard },
+}
+
+export const Mapform_Card = () => ({
+  components: { MapformCard },
+  template: `<MapformCard>
+  </MapformCard>`,
+})
---
+++ src/stories/Cards/ProductFilter.stories.js
@@ -0,0 +1,14 @@
+import ProductFilter from '../../components/Cards/ProductFilter'
+
+export default {
+  title: 'Cards/ProductFilter',
+  component: { ProductFilter },
+  argTypes: {
+    BackgroundColor: { control: 'color' },
+  },
+}
+
+export const Product_Filter = () => ({
+  components: { ProductFilter },
+  template: `<ProductFilter></ProductFilter>`,
+})
